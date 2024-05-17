316d4e7241538a6085aa80f360a846a110aafb7a
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-05-17 16:54:40 -0400

    Populating ef table

---
+++ Migrations/20240517203430_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                " VALUES('Coca-Cola', 'Refrigerante de cola', 5.45, 'coca.jpg', 50, GETDATE(), 1)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                               " VALUES('Lanche Atum', 'Lanche de Atum com Maionese', 12.00, 'atum.jpg', 30, GETDATE(), 2)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                                              " VALUES('Pudim 100 g', 'Pudim de leite condensado', 15.00, 'pudim.jpg', 10, GETDATE(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
